fc57a1f6d80abbd0d7c2232383a7817da61163fd
Clark <81452611+freezenleo@users.noreply.github.com>
2021-09-15 14:55:14 -0500

    setup gallery component

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import PhotoList from "../PhotoList";
+
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
+    return (
+        <section>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList />
+
+            {/* <div>
+                <img
+                    src={photo}
+                    alt="Commercial Example"
+                    className="img-thumbnail mx-1"
+                />
+            </div> */}
+        </section>
+    )
+}
+
+export default Gallery;
